e956163b8e42d8cc37f2ef5a056e993b87995fc4
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2025-06-10 01:09:30 +0330

    start threading

--- BlackBox/Program.cs
+++ BlackBox/Program.cs
@@ -1,12 +1,4 @@
-﻿using System.Globalization;
+﻿using BlackBox;
+using System.Globalization;
 
-string time = "September 14 2025";
-string format = "MMM dd yyyy";
-if (DateTime.TryParse(time,  out var parseDate))
-{
-    Console.WriteLine(parseDate.ToString("yyyy-MM-dd"));
-}
-else
-{
-    Console.WriteLine("invalid format");
-}
+WorkingThreading.ThreadMain();
---
+++ BlackBox/WorkingThreading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBox
+{
+    public class WorkingThreading
+    {
+        public static void ThreadMain()
+        {
+            Thread thread = new Thread(PrintNumber);
+            thread.Start();
+
+            for(int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"Main thread: {i}");
+                Thread.Sleep(500);
+            }
+            thread.Join();
+            Console.WriteLine("Main thread finished.");
+
+        }
+
+        public static void PrintNumber()
+        {
+            for(int i = 0; i <5; i++)
+            {
+                Console.WriteLine($"Worker thread: {i}");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
